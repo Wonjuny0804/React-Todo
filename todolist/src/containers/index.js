7ce8d7d2e5ed4637d5250ae73268b8ebfcab3214
WonjunJang <wonjun84@naver.com>
2021-04-05 15:07:01 +0900

    fix: Fix Containers/index.js

--- todolist/src/containers/index.js
+++ todolist/src/containers/index.js
@@ -2,6 +2,4 @@ export { default as TodoItem } from './TodoItem/TodoItem';
 export { default as DailyTodoList } from './DailyTodoList/DailyTodoList';
 export { default as WeeklyTodoList } from './WeeklyTodoList/WeeklyTodoList';
 export { default as Dialog } from './Dialog/Dialog';
-export { default as DailyTodoList } from './DailyTodoList/DailyTodoList'
-export { default as Header } from './Header/Header'
-export { default as WeeklyTodoList } from './WeeklyTodoList/WeeklyTodoList';
+export { default as Header } from './Header/Header';
