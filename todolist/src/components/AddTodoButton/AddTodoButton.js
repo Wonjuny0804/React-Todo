1d56b21419db3c177ea69ab7c3f1340235263b2d
Bernese-Corgi <jnyng0621@gmail.com>
2021-04-05 16:24:20 +0900

    feat: Add modal dialog component function in TodoListApp

--- todolist/src/components/AddTodoButton/AddTodoButton.js
+++ todolist/src/components/AddTodoButton/AddTodoButton.js
@@ -5,14 +5,31 @@ import {
   addTodoButton,
   plusIcon,
 } from 'components/AddTodoButton/AddTodoButton.module.scss';
+import ModalDialog from 'containers/ModalDialog/ModalDialog';
+import { Dialog } from 'containers';
 
 export default function AddTodoButton() {
+  const [isDialogShow, setIsDialogShow] = React.useState(false);
+  // const [dialogLabel, setDialogLabel] = React.useStateCallback('');
+
+  const showDialog = () => {
+    setIsDialogShow(true);
+  };
+  const hideDialog = () => {
+    setIsDialogShow(false);
+  };
+  const addTodoDialog = () => {
+    showDialog();
+    // setDialogLabel('Add Todo');
+  };
+
   return (
     <>
-      <button className={addTodoButton}>
+      <button className={addTodoButton} onClick={addTodoDialog}>
         <Heading level={3} children="Add a task" />
         <Button type="button" shape="plus" className={plusIcon} />
       </button>
+      <ModalDialog visible={isDialogShow} />
     </>
   );
 }
--- todolist/src/containers/ModalDialog/ModalDialog.js
+++ todolist/src/containers/ModalDialog/ModalDialog.js
@@ -1,64 +1,19 @@
+import React from 'react';
 import Portal from 'components/Portal/Portal';
 import { Dialog } from 'containers';
-import { AnimatePresence } from 'framer-motion';
-// import { AnimatePresence } from 'framer-motion';
-import React, { useEffect } from 'react';
+import './ModalDialog.scss';
 
-export default function ModalDialog({
-  hasModal = true,
-  visible = false,
-  className = '',
-  onOpen = null,
-  onClose = null,
-  onDimClickClose = null,
-  portalId = 'dialog-container',
-  children,
-  ...restProps
-}) {
-  const dialogRef = React.useRef(null);
-  const [modalMode] = React.useState(hasModal);
+const dialogContainer = document.getElementById('dialog-container');
+dialogContainer.classList.add('dialogContainer');
 
-  useEffect(() => {
-    if (visible) {
-      const reactRootNode = document.getElementById('root');
-      reactRootNode.setAttribute('aria-hidden', true);
-    }
-    // return () => {};
-  }, [visible]);
-
-  const dialog = {
-    hidden: {
-      opacity: 0,
-      x: '-50%',
-      y: '-150%',
-    },
-    visible: {
-      opacity: 1,
-      y: '-50%',
-    },
-  };
-
+export default function ModalDialog({ portalId = 'dialog-container' }) {
   return (
     <>
-      <Portal id={portalId} />
-      <AnimatePresence>
-        {visible && (
-          <div
-            role="dialog"
-            ref={dialogRef}
-            aria-modal={modalMode}
-            // className={classNames(styles.modal, className)}
-            tabIndex={-1}
-            key={dialog}
-            variants={dialog}
-            initial="hidden"
-            // animate="visible"
-            {...restProps}>
-            <Dialog headline="Add a task" buttonText="Add" />
-          </div>
-        )}
-        {/* {modalMode && visible && <Dialog.Modal onClose={handleClose} />} */}
-      </AnimatePresence>
+      <Portal id={portalId}>
+        <div role="dialog">
+          <Dialog headline="Add a task" buttonText="Add" />
+        </div>
+      </Portal>
     </>
   );
 }
