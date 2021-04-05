5c4ad19e07e8d576c2c5a552a3ab29d5c2d23465
ejinaaa <eunjink0423@gmail.com>
2021-04-05 10:57:22 +0900

    style: Create variable colors

--- todolist/src/components/index.js
+++ todolist/src/components/index.js
@@ -1,3 +1,3 @@
-export {default as Box} from './Box/Box'
-export {default as ContentBox } from './ContentBox/ContentBox'
-export {default as Checkbox } from './Checkbox/Checkbox'
+export { default as Box } from './Box/Box';
+export { default as ContentBox } from './ContentBox/ContentBox';
+export { default as Checkbox } from './Checkbox/Checkbox';
